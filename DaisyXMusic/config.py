3bbf93a0a045c571500622a6b2b1a6050c09ecfc
khayal-0508 <80122272+khayal-0508@users.noreply.github.com>
2021-08-30 16:44:30 +0400

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -1,30 +1,45 @@
+# DAISYXMUSIC- Telegram bot project
+# Copyright (C) 2021  Roj Serbest
+# Copyright (C) 2021  Inuka Asith
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Modified by Inukaasith
+
 import os
 from os import path
 from os import getenv
-
 from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = "AQDBG0VCJ02r3H2yY2Y9-YTpjsOlL3SF6U3MCG5_WieCP0OWHGCNsuGS1Tpb_UNTQ4wZKozRHW4sLqw-Ic_xqsyIt7agc_bX-xzPYBdjxeEOVJW3sWHAO3OyJ0egIrpuRQedMklRhlehliS4sPGvvlVod9lqCbLoq9skDqomxJWdWm_Rgt5bplXoBooK9a6lUN7q4NKaoFmCfr7BgCteuxTFXFGPpQiG1pV9sNL1HJj7qaPgPTHv-KreRmT8L5gBHxcfsl9XPnGSrOS3mz9TKcVQEJ4e7WHdS1NHy-I7baR5WfS-5yqHTVWiPzf6d-AzaeCGtE-vrs2Si3AevWpyOgXJXWo0nAA"
-BOT_TOKEN = "1780504797:AAE7R4gv0S44SJd0bI_BZFwt7GSfUHkhcag"
-BOT_NAME = "Khan Music Bot"
-UPDATES_CHANNEL = "KhanVlog"
-BG_IMAGE = "https://telegra.ph/file/dc0ed0f59cebc38749528.png"
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "KhanVlog")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dc0ed0f59cebc38749528.png")
 admins = {}
-API_ID = int("4453240")
-API_HASH = "543eea32f4ed971e6a7ced59d0e56aa2"
-BOT_USERNAME = "Khan_MusicBot"
-ASSISTANT_NAME = "KhanMusicAssistant"
-SUPPORT_GROUP = "KhanChat"
-PROJECT_NAME = "Khan Music Bot"
-SOURCE_CODE = "github.com/khayal-0508/DaisyXMusic"
-DURATION_LIMIT = int("15")
-ARQ_API_KEY = None
-PMPERMIT = "ENABLE"
-LOG_GRP = None
-COMMAND_PREFIXES = list("/")
-
-SUDO_USERS = list("1708421347")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "KhanMusicAssistant")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "KhanChat")
+PROJECT_NAME = getenv("PROJECT_NAME", "Khan Music Bot")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/khayal-0508/DaisyXMusic")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
